2219029d41a285055e40c5aae8a22ec505b75826
Abhinash02 <exampaperguide.in@gmail.com>
2026-05-08 17:06:59 +0530

    update CORN

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,10 +9,20 @@ const notFound = require("./middleware/notFoundMiddleware");
 const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://web-scrap-olive.vercel.app/", 
+];
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
